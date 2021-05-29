f9ff2682a8ca327e0cb4b1daa3f70a66c28b4d2f
Shashank Shekhar Harivyasi <harivyasi@gmail.com>
2021-05-29 21:58:46 +0100

    build book

---
+++ _build/jupyter_execute/executables/empty_notebook.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook exists as a placeholder. To be eventually removed.
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# In[ ]:
+
+
+
+
---
+++ _build/jupyter_execute/executables/main_notebook.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook exists as a placeholder. To be eventually overwritten.
+
+# In[1]:
+
+
+print("Hello World!")
+
+
+# In[ ]:
+
+
+
+
