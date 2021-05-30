58800276a02527df8268da8b0242e00ea40bd1e0
harivyasi <45761248+harivyasi@users.noreply.github.com>
2021-05-30 12:32:38 +0100

    testing file persistence

---
+++ _build/jupyter_execute/executables/calculate_molecule.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Calculate Molecule
+
+# In[1]:
+
+
+import ipywidgets as widgets 
+import pathlib
+import ipywidgets as widgets
+
+
+# In[3]:
+
+
+with open('try','r') as f:
+    x = f.readlines()
+
+print(x)
+
+
+# In[ ]:
+
+
+
+
